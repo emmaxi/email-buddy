6c684df473c204f045a05539c339cb574041a12d
Xi Wang <emmawangxi03@gmail.com>
2018-01-03 02:01:09 +0000

    initital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express'); //get access to express library
+const app = express();
+const PORT = process.env.PORT || 5000; //specific for Cloud9
+
+//route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'Emma'});
+});
+
+
+//when works on Cloud9, your port is process.env.PORT
+app.listen(PORT, function() {
+    console.log("APP is running on port " + process.env.PORT);
+});
