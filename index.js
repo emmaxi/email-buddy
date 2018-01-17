f7d425b010c0e5ed457436b33eda804fcc5aadef
Xi Wang <emmawangxi03@gmail.com>
2018-01-17 18:32:32 +0000

    partly google OAuth

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express'); //get access to express library
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
-const PORT = process.env.PORT || 5000; //specific for Cloud9
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken) => {
+            console.log(accessToken);
+        }
+    )
+);
 
 //route handler
-app.get('/', (req, res) => {
-    res.send({ bye: 'Buddy'});
-});
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {  //'google' string means use the GoogleStategy
+        scope: ['profile', 'email']  // scope means the info we asked from google
+    })
+);
 
 
+
+const PORT = process.env.PORT || 5000; //specific for Cloud9
 //when works on Cloud9, your port is process.env.PORT
 app.listen(PORT, function() {
     console.log("APP is running on port " + process.env.PORT);
