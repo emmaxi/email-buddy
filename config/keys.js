fa804400ad1e596afb3b582f33bbafab156518b2
Xi Wang <emmawangxi03@gmail.com>
2018-01-19 01:27:06 +0000

    add key.js

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+//export code and make it available to other files inside of your application
+module.exports = {
+    googleClientID: '876018285968-aq7vftig4868d3bshglt8ui45sp9qpqm.apps.googleusercontent.com',
+    googleClientSecret: '7sH_QUdHduL05-DkhB7Z-Xdk'
+};
+
+
