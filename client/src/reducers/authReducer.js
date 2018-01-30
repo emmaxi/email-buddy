6b52fbed7864cf0865ac0e4c3cf347cfe9bb3f29
Xi Wang <emmaxi@ccs.neu.edu>
2018-01-29 18:03:45 -0800

    finish header with all routes

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,12 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
     switch (action.type) {
+        case FETCH_USER:
+            // if action.payload is an empty string, return false, otherwise, return action.payload
+            return action.payload || false;
         default:
             return state;
     }
 }
+
