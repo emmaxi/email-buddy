9ac2a8977d2c3448a2d70fcd0c13c89303c9410e
Xi Wang <emmaxi@ccs.neu.edu>
2018-01-31 21:54:01 -0800

    resolve conflicts in user.js

--- models/User.js
+++ models/User.js
@@ -4,12 +4,8 @@ const { Schema } = mongoose;
     // const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-<<<<<<< HEAD
     googleId: String,
     credits: { type: Number, default: 0}
-=======
-    googleId: String
->>>>>>> 1e13def6c1ee267979650973756dc609982c3d72
 });
 
 mongoose.model('users', userSchema);
